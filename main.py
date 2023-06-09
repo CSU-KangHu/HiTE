040a222183b51f85208402a8c74f3859139915bc
kanghu <kanghu@csu.edu.cn>
2023-06-09 20:45:38 +0800

    fix tir merge

--- main.py
+++ main.py
@@ -431,7 +431,8 @@ if __name__ == '__main__':
         cur_confident_helitron_path = tmp_output_dir + '/confident_helitron_' + str(ref_index) + '.fa'
         os.system('cat ' + cur_confident_tir_path + ' >> ' + confident_tir_path)
         os.system('cat ' + cur_confident_helitron_path + ' >> ' + confident_helitron_path)
-
+    rename_fasta(confident_tir_path, confident_tir_path, 'TIR')
+    rename_fasta(confident_helitron_path, confident_tir_path, 'Helitron')
 
     starttime = time.time()
     log.logger.info('Start step3: generate non-redundant library')
